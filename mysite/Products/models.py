32c55ff1f3db05ae96d48ad8a05e7b9ccc6c1b9c
Jream <yadavyatharth387@gmail.com>
2024-07-20 11:40:15 +0530

    making changes to settings.py in order to get the right path of the images

--- mysite/Products/models.py
+++ mysite/Products/models.py
@@ -4,4 +4,4 @@ class Products(models.Model):
     name=models.CharField(max_length=255)
     price=models.FloatField()
     quantity=models.IntegerField()
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = models.ImageField(upload_to='', blank=True, null=True)
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -17,8 +17,8 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/Products/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'Products','media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
